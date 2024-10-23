8d0b9c674bea5c194c849b10919a779d4d327865
Grace_TASSI <gracektassi@gmail.com>
2024-10-23 15:02:48 +0200

    Fin de chap 16:[ADD]

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -29,17 +29,7 @@ class EstateOffer(models.Model):
         default=False,
     )
 
-    status = fields.Selection(
-        selection=[
-            ("accepted", "Accepted"),
-            ("refused", "Refused"),
-        ],
-        string="Status",
-        copy=False,
-        default=False,
-    )
 
-    
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
  
